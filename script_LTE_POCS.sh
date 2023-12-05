fc9363a305087ea8d44e951459b94716549046fc
Carlos Segura <cseguramail@gmail.com>
2023-12-05 11:40:12 +0100

    Removing scenarios from repo

--- script_LTE_POCS.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-for i in {1..250}
-do
-	./waf --run "scratch/POCS/POCS --RunNum=$(($i))"
-done
--- script_LTE_RealSce.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-for i in {1..250}
-do
-	./waf --run "scratch/RealSce/RealSce --RunNum=$(($i))"
-done
