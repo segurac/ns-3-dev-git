7dd2babbfe04b5c5126454feea99635a1d369b67
Carlos Segura <cseguramail@gmail.com>
2024-06-04 14:52:42 +0200

    Call launch script with config file as parameter

--- launch_sim.sh
+++ launch_sim.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 
-for ((VAR=1; VAR<=250; VAR++))    
+if [ $# -eq 0 ]; then
+    CONFIG="scratch/LTEHandoverSim/RL4N_toy/toy_config.xml"
+else
+    CONFIG="$1"
+fi
+
+
+CONFIGFILE=$1
+
+
+for ((VAR=1; VAR<=250; VAR++))
 do
    #python3 ns3 run "scratch/LTEHandoverSim/LTE_environment --ConfigFile=scratch/LTEHandoverSim/RealSce/real_sce_config.xml --RunNum=$VAR"
    #python3 ns3 run "scratch/LTEHandoverSim/LTE_environment --ConfigFile=scratch/LTEHandoverSim/POC/poc_sb_config.xml --RunNum=$VAR"
-   python3 ns3 run "scratch/LTEHandoverSim/LTE_environment --ConfigFile=scratch/LTEHandoverSim/RL4N_toy/toy_config.xml --RunNum=$VAR"
+   python3 ns3 run "scratch/LTEHandoverSim/LTE_environment --ConfigFile=$CONFIG --RunNum=$VAR"
 done
